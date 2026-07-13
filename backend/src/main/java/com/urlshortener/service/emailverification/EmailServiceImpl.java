5b0fa024e35acb6d0430a4a0a2f7f8b06dac788a
Viratx11 <147901560+Viratx11@users.noreply.github.com>
2026-07-13 15:36:44 +0530

    sendgrid added

--- backend/src/main/java/com/urlshortener/service/emailverification/EmailServiceImpl.java
+++ backend/src/main/java/com/urlshortener/service/emailverification/EmailServiceImpl.java
@@ -1,9 +1,19 @@
 package com.urlshortener.service.emailverification;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,21 +21,34 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
-    private final JavaMailSender mailSender;
+    @Autowired
+    private SendGrid sendGrid;
 
-    @Async
     @Override
-    public void sendEmail(String to, String subject, String body) {
+    public void sendEmail(String to, String subject, String content) {
+        Email from = new Email("snaplink0107@gmail.com");
+        Email toEmail = new Email(to);
+        Content contentObj = new Content("text/html", content);
+        Mail mail = new Mail(from, subject, toEmail, contentObj);
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+        Request request = new Request();
+        try {
 
-        // Optional: you can also remove this line and let JavaMail use the
-        // authenticated account.
-        message.setFrom("snaplink0107@gmail.com");
-
-        mailSender.send(message);
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            System.out.println("DEBUG: Attempting to send email to " + to);
+            Response response = sendGrid.api(request);
+            System.out.println("DEBUG: SendGrid Status Code: " + response.getStatusCode());
+            System.out.println("DEBUG: SendGrid Response Body: " + response.getBody());
+            System.out.println("DEBUG: SendGrid Headers: " + response.getHeaders());
+            if (response.getStatusCode() >= 300) {
+                System.out.println("DEBUG: !!! SENDGRID RETURNED AN ERROR !!!");
+            } else {
+                System.out.println("DEBUG: SendGrid API call accepted (202).");
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to send email", ex);
+        }
     }
 }
